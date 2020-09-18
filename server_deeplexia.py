a4b2c44430902f6316cc1a949664b8aff915eefb
elenche95 <elena.stein95@gmail.com>
2020-09-18 18:09:58 +0200

    testing heroku

--- server_deeplexia.py
+++ server_deeplexia.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from help_functions import *
+#from help_functions import *
 
 app = Flask(__name__)
 
@@ -8,12 +8,12 @@ def index():
    if request.method == 'POST':
       if request.form.get('sentences') == 'Sentences':
          text = request.form['text']
-         emojis = translate_text(text)
-         return render_template("index.html", result = emojis, origin_text = text)
+         #emojis = translate_text(text)
+         return render_template("index.html", result = 'emojis', origin_text = 'text')
       elif request.form.get('keywords') == 'Keywords':
          text = request.form['text']
-         emoji_text = translate_by_keywords(text)
-         return render_template("index.html", result= emoji_text, origin_text = text)
+        # emoji_text = translate_by_keywords(text)
+         return render_template("index.html", result= 'emoji_text', origin_text = 'text')
 
       elif request.form.get('reset') == 'Reset':
          return render_template("index.html", result='')
